f3c747c1a0131957f6be157b104697d91fe97654
osr-corgan <osr-corgan@eqdkp-plus.eu>
2008-07-26 16:15:06 +0000

    Moved remotely

---
+++ lastitems/language/german.php
@@ -0,0 +1,19 @@
+<?php
+/******************************
+ * EQDKP PLUS
+ * (c) 2008 by EQDKP Plus Dev Team
+ * http://www.eqdkp-plus.com
+ * ------------------
+ * $Id$
+ ******************************/
+
+if ( !defined('EQDKP_INC') ){
+    header('HTTP/1.0 404 Not Found');exit;
+}
+
+
+$plang = array_merge($plang, array(
+  'lastitems'            => 'Letzte Gegenstände',
+  'pk_last_items_limit'   => 'Anzeige Limit der letzten Items ',
+));
+?>
---
+++ lastitems/module.php
@@ -0,0 +1,69 @@
+<?php
+/******************************
+ * EQDKP PLUS Portal
+ * LasItems Module 
+ * (c) 2008 by EQDKP Plus Dev Team
+ * http://www.eqdkp-plus.com
+ * ------------------
+ * $Id$
+ ******************************/
+
+if ( !defined('EQDKP_INC') ){
+    header('HTTP/1.0 404 Not Found');exit;
+}
+
+
+$portal_module['lastitems'] = array(
+			'name'			    => 'LastItems Module',
+			'path'			    => 'lastitems',
+			'version'		    => '1.0.0',
+			'author'        => 'Corgan',
+			'contact'		    => 'http://www.eqdkp-plus.com',
+			'description'   => 'Show last items',
+			'positions'     => array('left1', 'left2', 'right'),
+      'install'       => array(
+                            'autoenable'        => '1',
+                            'defaultposition'   => 'right',
+                            'defaultnumber'     => '2',
+                          ),
+    );
+
+$portal_settings['lastitems'] = array(
+  'pk_last_items_limit'     => array(
+        'name'      => 'pk_last_items_limit',
+        'language'  => 'pk_last_items_limit',
+        'property'  => 'text',
+        'size'      => '2',
+      ),
+);
+
+if(!function_exists(lastitems_module)){
+  function lastitems_module(){
+  	global $eqdkp_root_path , $user, $eqdkp,$tpl,$conf_plus,$html, $plang;
+
+		include_once($eqdkp_root_path.'pluskernel/bridge/bridge_class.php');
+		$br = new eqdkp_bridge();
+		$limit = ($conf_plus['pk_last_items_limit'] > 0) ? $conf_plus['pk_last_items_limit'] : '5' ;
+		$lastitems = $br->get_last_items($limit);
+
+		if (is_array($lastitems))
+		{
+			$out = '<table width="100%" border="0" cellspacing="1" cellpadding="2" class="noborder">';
+
+			foreach ($lastitems as $item)
+			{
+				$class = $eqdkp->switch_row_class();
+				$out .= '<tr class="'.$class.'" nowrap onmouseover="this.className=\'rowHover\';" onmouseout="this.className=\''.$class.'\';">'.
+							"<td>
+								<a href='".$eqdkp_root_path."viewitem.php?i=". $item['id']."'>".$html->itemstats_item(stripslashes($item['name']),false,false)."</a> <br>".
+								get_coloredLinkedName($item['looter']).' ('.$item['value'].' DKP)
+							</td>
+						</tr>';
+			}
+			$out .= '</table>';
+
+			return $out;
+		}
+  }
+}
+?>
