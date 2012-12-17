c695951dc804b1a261770b14b63d248dcd243d02
godmod <godmod@eqdkp-plus.eu>
2012-12-17 14:06:34 +0000

    lastitems module: create tag, use trunk folder

---
+++ language/german.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * Project:     EQdkp-Plus
+ * License:     Creative Commons - Attribution-Noncommercial-Share Alike 3.0 Unported
+ * Link:		http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * -----------------------------------------------------------------------
+ * Began:       2008
+ * Date:        $Date: 2010-07-23 11:55:58 +0200 (Fr, 23. Jul 2010) $
+ * -----------------------------------------------------------------------
+ * @author      $Author: Godmod $
+ * @copyright   2006-2008 Corgan - Stefan Knaak | Wallenium & the EQdkp-Plus Developer Team
+ * @link        http://eqdkp-plus.com
+ * @package     eqdkp-plus
+ * @version     $Rev: 8439 $
+ * 
+ * $Id: german.php 8439 2010-07-23 09:55:58Z Godmod $
+ */
+
+if ( !defined('EQDKP_INC') ){
+    header('HTTP/1.0 404 Not Found');exit;
+}
+
+
+$plang = array_merge($plang, array(
+  'lastitems'            => 'Letzte Gegenstände',
+  'pk_last_items_limit'   => 'Anzeige Limit der letzten Items ',
+));
+?>
---
+++ language/spanish.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!defined('EQDKP_INC')) {
+	die('You cannot access this file directly.');
+}
+
+//Language: Spanish	
+//Created by EQdkp Plus Translation Tool on  2010-07-09 15:10
+//File: module_lastitems
+//Source-Language: english
+
+$plang = array_merge($plang, array(
+"lastitems" => "Últimos objetos",
+"pk_last_items_limit" => "Limitar los objetos",
+ ));
+
+?>
