634b70c527e84e065c287ef021fbb7c5499cdc6c
godmod <godmod@eqdkp-plus.eu>
2013-01-08 18:39:50 +0000

    added files for lastitems module to tag

---
+++ language/german.php
@@ -0,0 +1,31 @@
+<?php
+ /*
+ * Project:		EQdkp-Plus
+ * License:		Creative Commons - Attribution-Noncommercial-Share Alike 3.0 Unported
+ * Link:		http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * -----------------------------------------------------------------------
+ * Began:		2008
+ * Date:		$Date: 2012-07-01 14:25:03 +0200 (So, 01. Jul 2012) $
+ * -----------------------------------------------------------------------
+ * @author		$Author: godmod $
+ * @copyright	2006-2011 EQdkp-Plus Developer Team
+ * @link		http://eqdkp-plus.com
+ * @package		eqdkp-plus
+ * @version		$Rev: 11839 $
+ * 
+ * $Id: german.php 11839 2012-07-01 12:25:03Z godmod $
+ */
+
+if ( !defined('EQDKP_INC') ){
+    header('HTTP/1.0 404 Not Found');exit;
+}
+
+
+$lang = array(
+	'lastitems'				=> 'Letzte Gegenstände',
+	'lastitems_name'		=> 'Letzte Gegenstände',
+	'lastitems_desc'		=> 'Zeigt die letzten erhaltenen Gegenstände an',
+	'pk_last_items_limit'	=> 'Anzeige Limit der letzten Items ',
+	'pk_last_items_noitems' => 'Keine Gegenstände vorhanden',
+);
+?>
