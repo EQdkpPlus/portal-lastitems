19d4d114f56dc89428127b5dc63b5ad205780df5
godmod <godmod@eqdkp-plus.eu>
2013-09-24 12:29:14 +0000

    fixes

--- lastitems_portal.class.php
+++ lastitems_portal.class.php
@@ -62,8 +62,8 @@ class lastitems_portal extends portal_generic {
 				$buyer = $this->pdh->get('item', 'buyer', array($item));
 				$out .= '<tr class="nowrap">'.
 								"<td>
-									".$this->pdh->get('item', 'link_itt', array($item, $this->routing->build('item', false, false, false), '', false, false, false,false,false,true)).' <br />'.
-									$this->pdh->get('member', 'html_memberlink', array($buyer, $this->routing->build('character', false, false, false), '', false, false, true, true));
+									".$this->pdh->get('item', 'link_itt', array($item, $this->routing->simpleBuild('item'), '', false, false, false,false,false,true)).' <br />'.
+									$this->pdh->get('member', 'html_memberlink', array($buyer, $this->routing->simpleBuild('character'), '', false, false, true, true));
 					if (!$this->config->get('pk_disable_points')){
 									$out .= ' ('.$this->pdh->get('item', 'html_value', array($item))." ".$this->config->get('dkp_name').")";
 					}
