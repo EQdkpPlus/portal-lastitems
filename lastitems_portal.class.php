42cadcdb7591e1286e75b011425ea27feee2b2ac
God Mod <godmod23@web.de>
2015-01-14 11:36:20 +0100

    enable points

--- lastitems_portal.class.php
+++ lastitems_portal.class.php
@@ -64,7 +64,7 @@ class lastitems_portal extends portal_generic {
 								"<td>
 									".$this->pdh->get('item', 'link_itt', array($item, $this->routing->simpleBuild('items'), '', false, false, false,false,false,true)).' <br />'.
 									$this->pdh->get('member', 'html_memberlink', array($buyer, $this->routing->simpleBuild('character'), '', false, false, true, true));
-					if (!$this->config->get('disable_points')){
+					if ($this->config->get('enable_points')){
 									$out .= ' ('.$this->pdh->get('item', 'html_value', array($item))." ".$this->config->get('dkp_name').")";
 					}
 				$out .=	"			</td>
