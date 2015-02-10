e3576d8fcdda9b5000e4614c6d00806f562a48b9
GodMod <GodMod@users.noreply.github.com>
2015-02-10 09:55:04 +0100

    Update lastitems_portal.class.php

--- lastitems_portal.class.php
+++ lastitems_portal.class.php
@@ -28,7 +28,7 @@ class lastitems_portal extends portal_generic {
 	protected static $path		= 'lastitems';
 	protected static $data		= array(
 		'name'			=> 'LastItems Module',
-		'version'		=> '3.0.0',
+		'version'		=> '3.0.1',
 		'author'		=> 'Corgan',
 		'icon'			=> 'fa-trophy',
 		'contact'		=> EQDKP_PROJECT_URL,
